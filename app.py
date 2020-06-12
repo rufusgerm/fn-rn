feb29e9d099479395f8378598443bdec17703fcf
Jeremy Bergevin <jdbergevin@mac.com>
2020-06-12 15:33:34 -0700

    Adding functions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
+from nltk.corpus import stopwords
 from sklearn.ensemble import VotingClassifier
 
 
@@ -34,3 +35,13 @@ def about():
 
 if __name__ == '__main__':
     app.run(host="localhost", debug=True)
+
+def clean_string(string):
+    """
+    1. Tokenize Words
+    2. Remove Punctuation
+    3. Remove Stop Words
+    """
+    nopunc_list = (''.join(char for char in string if char not in my_punc)).split()
+    del_stopwords = [word for word in nopunc_list if word.lower() not in stopwords.words('english')]
+    return del_stopwords
