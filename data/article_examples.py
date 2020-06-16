a467f5bd47af370aa523cebe1f37f58fbaed0c72
Jeremy Bergevin <jdbergevin@mac.com>
2020-06-15 19:51:48 -0700

    Added articles. Truncated the navbar.

--- data/article_examples.py
+++ data/article_examples.py
@@ -2,11 +2,103 @@ sample_articles = [
     {
         'title': "U.S. economy will start to rebound in second half of 2020, Fed's Kaplan says",
         'text': "NEW YORK (Reuters) - The U.S. economy will experience a “significant, historic” contraction in the second quarter before it starts to rebound, and unemployment will remain elevated at the end of 2020, Dallas Federal Reserve President Robert Kaplan said on Monday. Kaplan said he projects that gross domestic product will drop by an annualized rate of 35% to 40% in the second quarter and that it should start to recover in the second half of the year. He said the economy may recover more quickly if consumers and businesses take precautions to limit the spread of the novel coronavirus. “If people wear masks widely, if we have extensive testing and contact tracing, if businesses and us as individuals follow good procedure, we’re going to grow faster,” Kaplan said during a virtual discussion organized by the Money Marketeers of New York University. The U.S. unemployment rate may have peaked and should decline over the summer, but could remain high near 8% at the end of the year, he said. Kaplan said he expects inflation to remain muted over the next couple of years even though some food prices are rising. U.S. Federal Reserve officials need to continue discussing the possibility of using yield-curve control, in which the central bank would target interest rates at specific maturities, Kaplan said. Bond market players are increasingly convinced one of the Fed’s next moves will be to cap yields at a specific point on the curve. “I have certain concerns about it, but I wouldn’t rule it out” said Kaplan, adding that he wants to make sure the Fed doesn’t create “more distortions” in financial markets.",
-        'URL': "https://www.reuters.com/article/us-usa-fed-kaplan/u-s-economy-will-start-to-rebound-in-second-half-of-2020-feds-kaplan-says-idUSKBN23M2DZ"
+        'URL': "https://www.reuters.com/article/us-usa-fed-kaplan/u-s-economy-will-start-to-rebound-in-second-half-of-2020-feds-kaplan-says-idUSKBN23M2DZ",
+        'Veracity': 'Real'
+    },
+    {
+        'title': "America’s Newsrooms Face a Reckoning on Race After Floyd Protests",
+        'text:' """In an online town hall earlier this month, staffers at Bloomberg News expressed concerns that the news organization hadn’t been aggressive enough in its coverage of the protests stemming from George Floyd’s killing.
+Top editors said covering such events wasn’t a strong suit for Bloomberg, whose core focus is business, according to a recording of portions of the meeting. They noted that the protests started in Minneapolis, a city where Bloomberg doesn’t have an office, and said customers that use Bloomberg’s terminals for news and financial information have access to coverage from other major outlets.
+“I don’t think decisions to cover what are essentially crime scenes on the ground reflects insensitivity to the news value of what’s going on,” Marty Schenker, chief content officer at Bloomberg News, said in the meeting, according to the recording.
+Some staffers were put off by the response, people familiar with newsroom discussions said. A Bloomberg spokeswoman said the company hasn’t received any complaints regarding the comments. Mr. Schenker declined to comment.
+News organizations have dealt with a host of difficult questions regarding race and diversity in the wake of the protests sparked by the death of Mr. Floyd, a black man who was killed last month when a Minneapolis police officer kneeled on his neck.
+From newspapers to cable news channels, the events have fueled debates in newsrooms over the extent and nature of protest coverage, how much editorial staffers should be able to express themselves publicly, news outlets’ commitment to diversity and conditions in the workplace for minorities.
+At The Wall Street Journal, more than 150 journalists on Friday sent a letter to Almar Latour, the recently appointed chief executive of News Corp unit Dow Jones and publisher of the Journal, and Matt Murray, editor in chief of the Journal, expressing concerns about diversity, hiring and coverage, among other issues, according to people familiar with the letter.
+“Many WSJ journalists, including many who are white, find the way we cover race to be problematic,” the letter says, according to a person who viewed it. The letter raised concerns that there aren’t enough people of color at the Journal, including in the top leadership, and said a lack of diversity is hindering the paper’s ability to tell a full range of stories about the country.
+In a memo to staff Monday, Mr. Latour said feedback from hundreds of employees has “included highly personal accounts, painful experiences and concerns about racial diversity in our workforce,” as well as concerns about pay transparency.  “I share the sentiment, as one colleague put it, that ‘this normal is not OK’ at Dow Jones,” Mr. Latour wrote.
+He said he has initiated a review of the issues raised by staffers and said the newsroom will pursue changes under Mr. Murray. A Dow Jones spokeswoman said Mr. Murray has been in touch with newsroom staff about their concerns.
+
+Similar debates over diversity are playing out at the Washington Post and the Los Angeles Times. At the New York Times, staff members revolted over the publication of an opinion piece by Republican Sen. Tom Cotton that called for the use of federal troops to help deter looting. James Bennet resigned as the Times’ editorial page chief after the paper said the piece wasn’t up to its standards.
+Elsewhere, pushback by journalists at the Philadelphia Inquirer over a headline that read, “Buildings Matter, Too,” led to the resignation of former top news editor Stan Wischnowski.
+Some journalists want more freedom to express their views on social media or attend protests and rallies. In many newsrooms, including the Journal’s, social-media guidelines for staff members are meant to avoid any perception that reporting is biased. Whether a journalist’s commentary or activities constitutes a political statement—or simply a statement of social values—can sometimes be a subject of debate.
+At CNN, network president Jeff Zucker was asked at a recent town hall why most editorial staffers can’t express their views on social media but the channel’s anchors can. He responded that anchors are treated differently,  according to people familiar with the meeting.
+Mr. Zucker said at the meeting the network was reviewing its restrictions on participating in protests. In a memo, CNN said that employees could donate to nonprofits but should refrain from taking part in protests the network was covering and from editorializing on social media. They can attend community vigils, CNN said in the memo.
+Mr. Zucker said at the meeting that he was sympathetic with staffers’ desire to protest. A CNN spokesman said employees are generally expected to avoid saying anything on social media that they wouldn’t say on air.
+In the Bloomberg town hall, Mr. Schenker took questions through a moderator alongside Editor in Chief John Micklethwait and Deputy Editor in Chief Reto Gregori. The moderator said one staffer noted that while Bloomberg requires journalists to be objective, “this feels different because there are clear distinctions between right and wrong,” according to the recording.
+
+Mr. Schenker said, “To me this doesn’t feel different in terms of being a journalist and being able to compartmentalize your personal feelings and your role as a journalist.” He added, “We have to be objective, state the facts, use our best judgment and not inject our own personal bias in our coverage.”
+Prior to the recent protests, there had already been some tension at Bloomberg over the perception that Mr. Micklethwait, who was named editor in chief in 2015, has played down coverage of nonbusiness topics, according to people familiar with newsroom discussions.
+Some reporters felt Bloomberg News was too slow to cover Mr. Floyd’s death and the protests. Mr. Micklethwait praised the coverage the organization did do, while saying the outlet could have done more, including appointing a news czar on the topic.
+A spokeswoman for Bloomberg News said that since Mr. Floyd’s death, Bloomberg has featured more than 1,200 related stories on the front page of the Bloomberg Terminal from a variety of sources, including Bloomberg News.
+“From the beginning I’ve said that our concentration should be on business, finance, politics and power and that’s the zone which we chose to compete on,”Mr. Micklethwait said, according to the recording.
+In a memo on Monday, Mr. Micklethwait said Bloomberg News needed to focus even more on equality, noting that “the issues of equality touch every reporting beat—and our global coverage needs to reflect that.” """,
+        'URL': "https://www.wsj.com/articles/americas-newsrooms-face-a-reckoning-on-race-after-floyd-protests-11592256570?mod=hp_lead_pos3",
+        'Veracity': 'Real'
+    },
+    {
+        'title': "The Real Dangers of Surveillance",
+        'text': """ How citizens and authorities respond to one another during large-scale protests — including how they use technology as a tool in the battle — can say a lot about trust in the entire political system.
+Paul Mozur, who has written extensively about the Chinese state surveillance machine for The New York Times, told me that mistrust of the authorities in Hong Kong was reflected in constant digital surveillance and paranoia during the pro-democracy demonstrations that started a year ago and continue to break out.
+Paul and I talked about how police and protesters used surveillance technology during the height of the Hong Kong protests last year, and what Americans — now gripped by their own protests — can learn from it.
+Shira: What were Hong Kong protesters’ worries about digital surveillance?
+Paul: They were constantly trying to avoid being tracked by law enforcement, although it wasn’t clear what the authorities were doing. People obsessively covered up with face masks, and this was before the coronavirus. Protesters avoided using subway cards that linked to their identity. To disrupt monitoring, protesters smashed every security camera they could, and some flashed laser pointers at police cameras.
+There was a suspicion that police filmed every demonstration to identify people through facial recognition software and target them. It wasn’t conclusively shown that was happening. But Hong Kongers took extreme measures because they were afraid of China’s government next door and its use of technology to watch and intimidate people.
+How did demonstrators use technology?
+There were protest channels on the messaging app Telegram to identify police officers who seemed to be overly aggressive during demonstrations. Some people publicly posted personal information about officers or their relatives, in an act known as doxxing. From the beginning, police took off identification badges, which drove the doxxing campaign.
+This sounds like constant chaos and fear.
+Both sides were fighting a surveillance war, and trying to shift the surveillance advantage toward themselves and cut off the other side. Though protesters were usually outmatched.
+How does that compare to what’s happening in the United States?
+I’m not there, so I might be missing things, but the U.S. protesters don’t seem to have the same level of paranoia about law-enforcement surveillance.
+If we start seeing U.S. protesters getting so afraid that they’re all wearing masks to shield their faces, or not driving to protests because they’re worried about license plate scanners, or not using credit cards, that would be a sign that U.S. protesters have lost trust in legal systems designed to protect their privacy. That’s what happened in Hong Kong.
+Isn’t there an argument that keeping people safe justifies surveillance?
+That’s the argument that gets made in China. But Hong Kong made it clear that to have civil liberties, a level of privacy is necessary. Without proper regulation, sweeping surveillance invites the potential abuse of power.
+The tools are there for the U.S. government to digitally track who is at these protests. Hopefully U.S. legal safeguards stop the tools from being abused. But it’s worth worrying about.
+Would you want a digital record of your presence at a political march, particularly if it could bring reprisal? In Hong Kong, that’s the fear people lived with.
+
+Amazon’s smaller slice of online shopping
+There’s more evidence that we’re trying online shopping outlets other than Amazon during the pandemic.
+Race and Policing ›
+Recent Changes Sparked by the Protests
+Updated June 15, 2020
+	•	Widespread protests in the aftermath of George Floyd’s killing by a Minnesota police officer have led to reckonings over enduring forms of racial discrimination in almost every sphere of American life. Among the changes so far:
+	•	Mayor Keisha Lance Bottoms of Atlanta issued a series of executive orders to overhaul the Atlanta Police Department’s use of force policies following the fatal shooting days earlier of Rayshard Brooks, a black man, by a white police officer. Earlier, New York became one of the first states to take action to restrict police forces, banning chokeholds and repealing a law that has kept police disciplinary records secret in the state.
+	•	The New York police commissioner announced that he was disbanding the anti-crime unit, a plainclothes team of hundreds of officers that targeted violent crime and was involved in some of the city’s most notorious police shootings. The officers will be reassigned to other duties.
+	•	Dozens of statues of historical figures associated with colonialism and slavery have been pulled down, beheaded, scorched or removed from their plinths in Britain, Belgium, New Zealand and the United States.
+	•	For the first time, the reality television show “The Bachelor” will have a black lead. The show, which kicked off in 2002, has long received criticism for its lack of diversity.
+
+I wrote in April that Americans had been spending less of our e-commerce money on Amazon than we typically did. I took another look at data through May, and that’s still largely the case. The pandemic might be permanently altering how and where we spend our money.
+Through May, out of each dollar of purchases that Americans made online, 35 to 37 cents of that went to Amazon, according to figures from Rakuten Intelligence. That’s a bit higher than it was in April, but we had been spending at least 42 cents of every e-commerce dollar on Amazon before the pandemic.
+It doesn’t sound like a big difference, but it is when we’re talking about the habits of many millions of people.
+What does this mean? Amazon has been the go-to online shopping stop for the biggest chunk of Americans, but that habit hasn’t stuck as much during the pandemic. This could be because of Amazon’s temporary delivery hiccups, our shift to buying more groceries during this time or other factors.
+Compared to the first couple months of 2020, people are spending a bigger chunk of their e-commerce budget with the grocery delivery service Instacart, Target, Home Depot and Best Buy, according to Rakuten, which has the permission of more than 1 million Americans to view their emailed purchase receipts.
+This news isn’t necessarily bad for Amazon. Americans have been shopping online more than ever during the pandemic. If this behavior sticks, Amazon is a big winner even if it takes a smaller slice from this much larger e-commerce pie.
+But for those of us who worry about a few companies taking too much power, spreading out the riches is probably a good thing.
+
+Before we go …
+	•	Twitter’s sweep against disinformation: Twitter is deleting more than 170,000 China-linked accounts that pushed disinformation about the coronavirus and other topics, my colleague Kate Conger reported. This step followed a Times analysis that found what seemed to be a coordinated online campaign to amplify the Chinese government’s views. It’s another reminder of the cat-and-mouse game to flesh out government-backed online propaganda campaigns.
+	•	Who decides what habits are good for us? A Japanese teenager is fighting against government limits on the amount of time that young people can spend playing video games or using the internet. My colleagues Ben Dooley and Hikari Hida write that concerns about children’s addiction to video games is clashing with discomfort about government intrusion into people’s personal lives.
+	•	Moving beyond “diversity theater”: The Washington Post writes about the ripple effects from the tiny number of black investors in the upper ranks of firms that back young technology companies. The start-up investment industry, the Post writes, is now confronting its track record on race and the dearth of investment money going to black founders of tech start-ups. """,
+        'URL': "https://www.nytimes.com/2020/06/12/technology/surveillance-protests-hong-kong.html",
+        'Veracity': 'Real'
     },
     {
         'title': "",
-        'text:' """ """,
+        'text': """ """,
+        'URL': ""
+    },
+    {
+        'title': "",
+        'text': """ """,
+        'URL': ""
+    },
+    {
+        'title': "",
+        'text': """ """,
+        'URL': ""
+    },
+    {
+        'title': "",
+        'text': """ """,
         'URL': ""
     }
 ]
